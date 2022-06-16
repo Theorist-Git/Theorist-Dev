20000c4b045d4d57d7a7c4a5886a2ab57da3ca72
_Theorist_ <mayankvats460@gmail.com>
2022-06-16 16:06:34 +0530

    tert.py updated with latest code

--- tertUP.py
+++ tertUP.py
@@ -16,13 +16,10 @@ use or distribute this software.
 """
 
 
-class OTPMethods:
-    # Getting systemRandom class instance out of the secrets module
-    def __init__(self):
-        from secrets import SystemRandom
-        self.secrets_generator = SystemRandom()
+class TwoFactorAuth:
 
-    def return_random(self, otp_len: int = 6) -> str:
+    @staticmethod
+    def static_otp(otp_len: int = 6) -> str:
         """
         Used to generate Random ONE-TIME-PASSWORDS which are
         utilized in the app for user verification and authentication.
@@ -32,10 +29,56 @@ class OTPMethods:
         :return: str(random integer in range [10^n, (10^n) - 1])
         """
         # secure random integer numbers
+        from secrets import SystemRandom
+
+        secrets_generator = SystemRandom()
         l_range = (10 ** (otp_len - 1))
         h_range = (l_range * 10) - 1
-        otp = self.secrets_generator.randint(l_range, h_range)
+        otp = secrets_generator.randint(l_range, h_range)
         return str(otp)
+
+    @staticmethod
+    def totp(name, issuer_name: str = "ArcisCoding.io", secret_len: int = 64) -> tuple:
+        from pyotp import random_base32, TOTP
+        token = random_base32(secret_len)
+        URL = TOTP(token).provisioning_uri(name=name, issuer_name=issuer_name)
+
+        return token, URL
+
+    @staticmethod
+    def verify(token: str, otp) -> bool:
+        from pyotp import TOTP
+        return TOTP(token).verify(str(otp))
+
+    @staticmethod
+    def encrypt(key: bytes, source: bytes, encode=True) -> str:
+        import base64
+        from Crypto.Cipher import AES
+        from Crypto.Hash import SHA256
+        from Crypto import Random
+        key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+        IV = Random.new().read(AES.block_size)  # generate IV
+        encryptor = AES.new(key, AES.MODE_CBC, IV)
+        padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
+        source += bytes([padding]) * padding  # Python 2.x: source += chr(padding) * padding
+        data = IV + encryptor.encrypt(source)  # store the IV at the beginning and encrypt
+        return base64.b64encode(data).decode("latin-1") if encode else data
+
+    @staticmethod
+    def decrypt(key: bytes, source, decode=True) -> str:
+        import base64
+        from Crypto.Cipher import AES
+        from Crypto.Hash import SHA256
+        if decode:
+            source = base64.b64decode(source.encode("latin-1"))
+        key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
+        IV = source[:AES.block_size]  # extract the IV from the beginning
+        decryptor = AES.new(key, AES.MODE_CBC, IV)
+        data = decryptor.decrypt(source[AES.block_size:])  # decrypt
+        padding = data[-1]  # pick the padding value from the end; Python 2.x: ord(data[-1])
+        if data[-padding:] != bytes([padding]) * padding:  # Python 2.x: chr(padding) * padding
+            raise ValueError("Invalid padding...")
+        return data[:-padding].decode('utf-8')  # remove the padding
 
 
 class ElectronicMail:
